051cb924437bfc349bc99872167113dfed9278b0
tari9925 <tadas.riksas@mif.stud.vu.lt>
2025-09-14 15:50:22 +0300

    Implement list transformation and arithmetic operations in Prolog

--- labs/lab3/lists.pl
+++ labs/lab3/lists.pl
@@ -48,8 +48,52 @@ skaiciuoti_pakartojimus([Pirmas | Like], Simbolis, Pasikartojimai, ReikalingiPas
 
 % R = [x,c,y].
 
+keisti(Sarasas, Keitiniai, Rezultatas) :- keitimas(Sarasas, Keitiniai, Rezultatas).
+
+keitimas([Pirmas | Like], K, [Pakeistas | Resto]) :-
+    paieska_keitimo(Pirmas, K, Pakeistas),
+    keitimas(Like, K, Resto).
+
+paieska_keitimo(Simbolis, [k(Taisykle, Pakeitimas) | Like], Pakeistas) :-
+    Simbolis == Taisykle,
+    Pakeistas = Pakeitimas.
+
+paieska_keitimo(Simbolis, [k(Taisykle, _) | Like], Pakeistas) :-
+    Simbolis \== Taisykle,
+    paieska_keitimo(Simbolis, Like, Pakeistas).
+    
 
 % 4.8 skirt(S1,S2,Skirt) - S1 ir S2 yra skaičiai, vaizduojami skaitmenų sąrašais. Skirt - tų skaičių skirtumas, vaizduojamas skaitmenų sąrašu. Laikykite, kad S1 yra ne mažesnis už S2. Pavyzdžiui:
 % ?- skirt([9,4,6,1,2,8],[3,4],Skirt).
 
 % Skirt = [9,4,6,0,9,4].
+
+
+i_skaiciu([], 0).
+i_skaiciu([H|T], Skaicius) :-
+    i_skaiciu(T, Dalis),
+    length(T, Ilgis),
+    Skaicius is H * 10^Ilgis + Dalis.
+
+is_skaicios(0, [0]).
+is_skaicios(Skaicius, Sarasas) :-
+    Skaicius > 0,
+    is_skaicios_akumuliatyvi(Skaicius, [], Sarasas).
+
+is_skaicios_akumuliatyvi(0, Akumuliatorius, Akumuliatorius).
+is_skaicios_akumuliatyvi(Skaicius, Akumuliatorius, Sarasas) :-
+    Skaicius > 0,
+    Skaicius1 is Skaicius // 10,
+    Skaicius2 is Skaicius mod 10,
+    is_skaicios_akumuliatyvi(Skaicius1, [Skaicius2 | Akumuliatorius], Sarasas).
+
+
+skirt(S1, S2, Skirt) :-
+    i_skaiciu(S1, Skaicius1),
+    i_skaiciu(S2, Skaicius2),
+    SkirtasSkaicius is Skaicius1 - Skaicius2,
+    is_skaicios(SkirtasSkaicius, Skirt).
+
+% Operacijos su natūraliaisiais skaičiais, išreikštais skaitmenų sąrašais. Skaitmenų sąrašo elementai turi būti natūralūs skaičiai nuo 0 iki 9 (ne simboliai '0', '1',...). Nenaudokite Prolog konvertavimo tarp sąrašo ir skaičiaus predikatų (number_chars/2, number_codes/2 ir kt...):
+
+% Questionable requirements for the task above, maybe rewrite it using simple arithmetics...????
